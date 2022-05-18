62470ca971ad2857959b24ef6066e57d7fb2488a
Alvi Mahadi <alvi.utsab@gmail.com>
2022-05-18 14:35:46 -0700

    add sender and recipient in message model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ApplicationRecord
-    belongs_to :sender, class_name: "User"
+    belongs_to :sender, class_name: "User", default: -> { Current.user }
     belongs_to :recipient, class_name: "User"
     has_many :messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,13 @@
 class Message < ApplicationRecord
     belongs_to :user, default: -> { Current.user }
     belongs_to :conversation
+
+    def sender
+        user
+    end
+
+    def recipient
+        return conversation.recipient if sender == conversation.sender
+        return conversation.sender if sender == conversation.recipient
+    end
 end
