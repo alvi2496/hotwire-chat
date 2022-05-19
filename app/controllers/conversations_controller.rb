2abcc898d3e251083d51b42ff88fc68f416bbcb2
Alvi Mahadi <alvi.utsab@gmail.com>
2022-05-19 10:10:02 -0700

    add converastion controllersa

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,9 @@
 class ConversationsController < ApplicationController
 
     before_action :authenticate_user!
+
+    def index
+        @conversations = Conversation.participating(Current.user)
+    end
     
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,6 @@ class Conversation < ApplicationRecord
     belongs_to :sender, class_name: "User", default: -> { Current.user }
     belongs_to :recipient, class_name: "User"
     has_many :messages
+
+    scope :participating, -> (user) { where("sender_id = ? OR recipient_id = ?", user.id, user.id) }
 end
